653246663cba1b7d4f19ea2eba5c027074ccbdf6
Luis Fernando Farji <fernando.farji@gmail.com>
2023-11-17 22:06:20 -0300

    counter

--- js/script.js
+++ js/script.js
@@ -23,3 +23,46 @@ home.onmouseleave = () =>{
 
   homeImg.style.transform= `translateX(0px) translateY(0px)` ;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let dataOffeset = (24*60*60*1000) * 5;
+let countDate = new Date().getTime() + dataOffeset;
+
+function countDown(){
+
+  let now = new Date().getTime()
+  let gap = countDate - now;
+
+  let seconds = 1000;
+  let minutes = seconds * 60;
+  let hours = minutes * 60;
+  let days = hours * 24;
+
+  let d = Math.floor(gap / (days));
+  let h = Math.floor((gap % (days)) / (hours));
+  let m = Math.floor((gap % (hours))/ (minutes));
+  let s =Math.floor ((gap % (minutes)) / (seconds));
+
+  document.getElementById('day').innerText = d;
+  document.getElementById('hour').innerText = h;
+  document.getElementById('minute').innerText = m;
+  document.getElementById('second').innerText =s;
+
+}
+
+setInterval(function(){
+  countDown();
+},1000)
