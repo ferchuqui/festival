c9e80e8efaec9b171edb1838513a155a9fd4baf3
Luis Fernando Farji <fernando.farji@gmail.com>
2023-11-19 20:59:59 -0300

    offers

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ MenuBtn.onclick = () => {
   MenuBtn.classList.toggle("fa-times");
 };
 
-window.onscroll = () =>{
+window.onscroll = () => {
   navbar.classList.remove("active");
   MenuBtn.classList.remove("fa-times");
 
@@ -16,7 +16,7 @@ window.onscroll = () =>{
   } else {
     header.classList.remove("active");
   }
-}
+};
 
 let home = document.querySelector("#home");
 
@@ -26,7 +26,7 @@ home.onmousemove = (e) => {
   let y = (window.innerHeight - e.pageY * 2) / 90;
 
   homeImg.style.transform = `translateX(${x}px) translateY(${y}px)`;
-}
+};
 
 home.onmouseleave = () => {
   let homeImg = home.querySelector(".home-img");
